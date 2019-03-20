8e4ae85fc2e37edbb9f4536e93fb7e91d6df0765
SAM ROBERTS <samueljustinroberts@gmail.com>
2019-03-20 01:29:10 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,28 +1,27 @@
 def addition(num1, num2)
-  puts 5+4
+ puts num1 + num2
 end
 
 def subtraction(num1, num2)
-  puts 10-5
+ 
 end
 
 def division(num1, num2)
-  puts 50/2
+  
 end
 
 def multiplication(num1, num2)
-  puts 4*30
+  
 end
 
 def modulo(num1, num2)
-  puts 34%5
+
 end
 
 def square_root(num)
-  puts math.sqrt(81)
+
 end
 
 def order_of_operation(num1, num2, num3, num4)
- puts 7 +((43*23))/83)
   #Hint:  __ + (( __ * __ ) / __ )
 end
